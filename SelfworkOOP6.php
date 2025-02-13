77a53bbcddd15716f67033744e05c00feb06e103
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2025-02-13 11:34:32 +0100

    Selfwork PHP OOP 6

---
+++ SelfworkOOP6.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'class.php';
+
+class Post {
+    private $titolo;
+    private $categoria;
+    private $tag;
+
+    public function __construct($titolo, Category $categoria, array $tag) {
+        $this->titolo = $titolo;
+        $this->categoria = $categoria;
+        $this->tag = $tag;
+    }
+
+    public function getPostDetails() {
+        echo "Titolo: " . $this->titolo . "<br>";
+        echo "Categoria: ";
+        $this->categoria->getMyCategory();
+        echo "<br>Tag: " . implode(", ", $this->tag) . "<br><br>";
+    }
+}
+
+// Creazione di istanze di Post
+$post1 = new Post("Ultime Notizie", new Attualita(), ["politica", "economia"]);
+$post2 = new Post("Finale di Champions League", new Sport(), ["calcio", "Champions League"]);
+$post3 = new Post("Nuovi Gossip", new Gossip(), ["celebrità", "rumors"]);
+$post4 = new Post("Eventi Storici", new Storia(), ["antichità", "guerre"]);
+
+// Visualizzazione dettagli
+$post1->getPostDetails();
+$post2->getPostDetails();
+$post3->getPostDetails();
+$post4->getPostDetails();
+
+?>
